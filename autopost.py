dc914c8a728d53e7614ba6a45d8018394664e678
omni- <cooper@stevetex.com>
2022-04-24 02:24:00 -0700

    rework logic, add flair support

--- autopost.py
+++ autopost.py
@@ -4,23 +4,24 @@ import sys
 import getopt
 
 def usage():
-    print("Usage: python autopost.py -s[--subreddit] <subreddit> -t[--title] <title> -p[--post] <post body> -x[--postTime] <time to post>")
+    print("Usage: python autopost.py -s[--subreddit] <subreddit> -t[--title] <title> -p[--post] <post body> -x[--postTime] <time to post> -f[--flair] <flair> -i[--spoiler] <is spoiler?>")
+    raise
 
 title = ''
 body = ''
-post_time = '5:30'
+post_time = '05:30'
 subname = ''
+flair = ''
+spoiler = False
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], 's:t:p:x:h', ['subreddit=', 'title=', 'post=', 'postTime=', 'help'])
+    opts, args = getopt.getopt(sys.argv[1:], 'f:i:s:t:p:x:h', ['subreddit=', 'title=', 'post=', 'postTime=', 'help'])
 except getopt.GetoptError:
     usage()
-    sys.exit(2)
 
 for opt, arg in opts:
     if opt in ('-h', '--help'):
         usage()
-        sys.exit(2)
     elif opt in ('-t', '--title'):
         title = arg
     elif opt in ('-p', '--post'):
@@ -29,14 +30,16 @@ for opt, arg in opts:
         post_time = arg
     elif opt in ('-s', '--subreddit'):
         subname = arg
+    elif opt in ('-f', '--flair'):
+        flair = arg
+    elif opt in ('-i', '--spoiler'):
+        spoiler = bool(arg)
     else:
         usage()
-        sys.exit(2)
 
 if title == '' or body == '' or subname == '':
     print('Subreddit, title and body required.')
     usage()
-    sys.exit(2)
 
 f = open('./creds.txt')
 
@@ -53,7 +56,7 @@ reddit = praw.Reddit(
     username=creds[0], 
     password=creds[1]
 )
-
+reddit.validate_on_submit = True
 sub = reddit.subreddit(subname)
 posted = False
 
@@ -62,16 +65,26 @@ if sub is None or sub.display_name == '':
     print("subreddit " + subname + " not found.")
     sys.exit(2)
 
+if sub.post_requirements()['is_flair_required'] and flair == '':
+    print('flair required for this subreddit.')
+    sys.exit(2)
+
 print('subreddit: ' + sub.display_name)
-print('description: ' + sub.description.split("\n")[0])
+print('first line of sub description: ' + sub.description.split("\n")[0])
 
 
 while not posted:
     now = time.localtime()
     current_time = time.strftime("%H:%M", now)
+    print('local time: ' + current_time)
     
-    if current_time is post_time:
-        sub.submit(title, body)
+    if current_time == post_time:
+        if flair == '':
+            sub.submit(title, selftext=body, spoiler=spoiler)
+        else:
+            choices = list(sub.flair.link_templates.user_selectable())
+            template_id = next(x for x in choices if x["flair_text"] == flair)["flair_template_id"]
+            sub.submit(title, selftext=body, flair_id=template_id, spoiler=spoiler)
         posted = True
         print('Posted!')
     else:
